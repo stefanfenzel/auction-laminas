c455284e706032cda57f508933b561e774827278
Stefan Fenzel <stefan.fenzel@web.de>
2024-12-08 16:54:46 +0100

    Move optional parameter after required + add login route

--- module/Auction/src/App/Users/Controller/AuthController.php
+++ module/Auction/src/App/Users/Controller/AuthController.php
@@ -4,9 +4,31 @@ declare(strict_types=1);
 
 namespace Auction\App\Users\Controller;
 
+use Laminas\Authentication\Adapter\DbTable\CredentialTreatmentAdapter;
+use Laminas\Authentication\AuthenticationService;
+use Laminas\Http\Response;
 use Laminas\Mvc\Controller\AbstractActionController;
+use Laminas\View\Model\ViewModel;
 
 final class AuthController extends AbstractActionController
 {
+    public function loginAction(): Response|ViewModel
+    {
+        $email = $this->params()->fromPost('email');
+        $password = $this->params()->fromPost('password');
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
+        $authAdapter = new CredentialTreatmentAdapter($email, $hashedPassword);
+
+        $auth = new AuthenticationService();
+        $result = $auth->authenticate($authAdapter);
+
+        if (! $result->isValid()) {
+            return new ViewModel([
+                'error' => 'Invalid credentials',
+            ]);
+        }
+
+        return $this->redirect()->toRoute('dashboard');
+    }
 }
